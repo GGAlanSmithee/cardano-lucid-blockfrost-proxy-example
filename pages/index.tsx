b5d1f84ad3f6ea465f10f4647b02a81e7d9c6680
Jesper Tullberg <jesper.tullberg@zenta.se>
2022-10-27 16:14:35 +0200

    small cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,7 +47,7 @@ const Index = () => {
       <br />
 
       <div>
-        <button onClick={() => sendTransaction()}>Send transaction</button>
+        <button onClick={sendTransaction}>Send transaction</button>
       </div>
     </div>
   )
