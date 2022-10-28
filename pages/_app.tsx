766318e5dae2eda94fb24e9361a1d79781b716e4
Jesper Tullberg <jesper.tullberg@zenta.se>
2022-10-28 17:38:34 +0200

    smoother background color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
-import type { AppProps } from 'next/app'
+import '../styles/styles.css';
+
+import type { AppProps } from "next/app"
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
