9e9e9c76a8344b37f554fc2809fd306d3ff59366
Jesper Tullberg <jesper.tullberg@zenta.se>
2022-11-07 15:25:02 +0100

    move back to having lucid as a state since if not, changes aren't picked up

--- hooks/use-lucid.ts
+++ hooks/use-lucid.ts
@@ -1,30 +1,33 @@
 import { isNil } from "lodash"
 import { Blockfrost, Lucid } from "lucid-cardano"
-import { useEffect } from "react"
+import { useCallback, useEffect, useState } from "react"
 
 import { useNetworkId } from "./use-network-id"
 import { useWalletApi } from "./use-wallet-api"
 
-const lucid = await Lucid.new()
-
 const useLucid = () => {
+  const [lucid, setLucid] = useState<Lucid>()
   const walletApi = useWalletApi()
   const networkId = useNetworkId(walletApi)
 
-  useEffect(() => {
+  const initializeLucid = useCallback(async () => {
     if (isNil(networkId) || isNil(walletApi)) return
 
-    // todo, check which of these values that have actually changed
-    lucid
-      .switchProvider(
-        new Blockfrost(`/api/blockfrost/${networkId}`),
-        networkId === 0 ? "Testnet" : "Mainnet"
-      )
-      .then(() => {
-        // for now, we always need to selectWallet after switching provider
-        lucid.selectWallet(walletApi)
-      })
-  }, [networkId, walletApi])
+    const provider = new Blockfrost(`/api/blockfrost/${networkId}`)
+    const network = networkId === 0 ? "Testnet" : "Mainnet"
+
+    const updatedLucid = await (isNil(lucid)
+      ? Lucid.new(provider, network)
+      : lucid.switchProvider(provider, network))
+
+    const lucidWithWallet = updatedLucid.selectWallet(walletApi)
+
+    setLucid(lucidWithWallet)
+  }, [lucid, networkId, walletApi])
+
+  useEffect(() => {
+    initializeLucid()
+  }, [initializeLucid])
 
   return {
     networkId,
