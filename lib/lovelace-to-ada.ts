f440436745dd7edd3a0a5fa7f5e35ae51fc5bddf
Jesper Tullberg <jesper.tullberg@zenta.se>
2023-01-01 15:49:19 +0100

    handle fractional lovelace

--- lib/lovelace-to-ada.ts
+++ lib/lovelace-to-ada.ts
@@ -1,3 +1,7 @@
-const lovelaceToAda = (lovelace: bigint | number) => Number(BigInt(lovelace) / 1000000n)
+const lovelaceToAda = (lovelace: bigint | number) => {
+  if (typeof lovelace === "number") lovelace = Math.round(lovelace)
+
+  return Number(BigInt(lovelace) / 1000000n)
+}
 
 export { lovelaceToAda }
